712dd578be05f28995c517966039d4337312acdc
OmarDIOP0 <odiop6170@gmail.com>
2025-03-20 18:25:45 +0000

    ajout_de_la_documentation_swagger

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API de Gestion des Classes",
+        default_version='v1',
+        description="Documentation de l'API de JanguXayma.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="janguxayma@exemple.com"),
+        license=openapi.License(name="ESP"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('authentication.urls'))
+    path('api/v1/',include('authentication.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
 ]
