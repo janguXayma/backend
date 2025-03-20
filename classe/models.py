e0339fb26681aa7c75d2669b1b7e04d990c31f87
OmarDIOP0 <odiop6170@gmail.com>
2025-03-20 18:21:18 +0000

    ajout_de_la_fonctionnalité_classe

--- classe/models.py
+++ classe/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from authentication.models import Student,Teacher
 from .utils import Utils
+import uuid
 
 # Create your models here.
 class Classe(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     code_activation = models.CharField(max_length=4, unique=True, 
                                        default=Utils.generate_activation_code,editable=False)
     description = models.TextField(blank=True, null=True)
     students = models.ManyToManyField(Student, related_name='classes', blank=True)
-    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='classes', null=True, blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='classes', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
--- classe/utils.py
+++ classe/utils.py
@@ -1,7 +1,11 @@
 import random
 import string
+from .models import Classe
 class Utils:
     @staticmethod
     def generate_activation_code():
         """Generate a random activation code of 4 characters."""
-        return ''.join(random.choices(string.ascii_uppercase, k=4))
+        while True:
+            code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+            if not Classe.objects.filter(activation_code=code).exists():
+                return code
