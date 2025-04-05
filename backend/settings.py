684ad0b088cd1fa5718656411c2433a0dda62a85
OmarDIOP0 <odiop6170@gmail.com>
2025-04-05 13:52:18 +0000

    ajout des infos

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.sites',
     'rest_framework',
     'corsheaders',
     'authentication',
--- classe/serializers.py
+++ classe/serializers.py
@@ -6,12 +6,13 @@ from authentication.serializers import StudentSerializer
 
 class ClasseSerializer(serializers.ModelSerializer):
     teacher = serializers.HiddenField(default = serializers.CurrentUserDefault())
+    teacher_name = serializers.CharField(source='teacher.user.username', read_only=True)
     name = serializers.CharField(required=True)
     students = StudentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Classe
-        fields = ['id','name', 'code_activation', 'description', 'students', 'teacher','created_at','updated_at']
+        fields = ['id','name', 'code_activation', 'description', 'students', 'teacher','created_at','updated_at','teacher_name']
         read_only_fields = ('code_activation', 'teacher', 'created_at', 'updated_at')
 
     def create(self, validated_data):
